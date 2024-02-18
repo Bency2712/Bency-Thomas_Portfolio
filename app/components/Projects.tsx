20a225a170b669bd90201bcd82cf0026cc097b67
Bency2712 <bencythomas6645@gmail.com>
2024-02-18 13:39:58 -0500

    Project

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -11,7 +11,7 @@ export default function Projects() {
             <div className="project-box"><a href="https://github.com/Bency2712/"><img src="/img/splitsmart.png" alt="" /></a><span>Splitsmart</span></div>
             <div className="project-box"><a href="https://github.com/Bency2712/Code-Smell-Detection-using-CNN"><img src="/img/code_smell_detection.png" alt="" /></a><span>Code smell detection</span></div>
             <div className="project-box"><a href="https://nest-quest-accomodation-booking.vercel.app"><img src="/img/nba.png" alt="" /></a><span>Data Analytics in NBA</span></div>
-            <div className="project-box"><a href="https://github.com/Bency2712/"><img src="/img/weather.png" alt="" /></a><span> Weather app</span></div>
+            <div className="project-box"><a href="https://weather-app-six-beryl.vercel.app/"><img src="/img/weather.png" alt="" /></a><span> Weather app</span></div>
             <div className="project-box"><a href="https://bency-thomas-pomodoro-timer.vercel.app/"><img src="/img/pomodoro.png" alt="" /></a><span>Pomodoro Timer</span></div>
         </div>
         </div>
